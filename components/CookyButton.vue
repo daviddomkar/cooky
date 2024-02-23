ef849b5d61b4f3b435b42df4d04b042bb2f7bf30
David Domkář <david@domkar.dev>
2024-02-24 00:08:28 +0100

    feat(linter): improve linter settings

--- components/CookyButton.vue
+++ components/CookyButton.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="w-40 h-10 rounded-[24px] bg-surface flex items-center box-border px-4"
+    class="box-border h-10 w-40 flex items-center rounded-[24px] bg-surface px-4"
   >
-    <icon class="w-8 h-8 i-game-icons-croissant" src="" />
+    <icon class="i-game-icons-croissant h-8 w-8" src="" />
     <span class="text-primary">Cooky</span>
   </div>
 </template>
