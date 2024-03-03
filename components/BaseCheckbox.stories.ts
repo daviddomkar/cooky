32909a67fca092dccc69df848a0b1a260ee952d4
Matyanson <matyas.perem@seznam.cz>
2024-03-03 20:38:30 +0100

    feat(components): add checkbox

---
+++ components/BaseCheckbox.stories.ts
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+
+import BaseCheckbox from "./BaseCheckbox.vue";
+
+const meta = {
+  title: "BaseCheckbox",
+  component: BaseCheckbox,
+} satisfies Meta<typeof BaseCheckbox>;
+
+export default meta;
+
+type Story = StoryObj<typeof BaseCheckbox>;
+
+export const Default: Story = {
+  args: {
+    name: "checkboy",
+    default: "Agree Terms & Conditions",
+  },
+  render: (args) => ({
+    setup() {
+      return { args };
+    },
+    components: { BaseCheckbox },
+    template: '<BaseCheckbox v-bind="args">{{ args.default }}</BaseCheckbox>',
+  }),
+};
---
+++ components/BaseCheckbox.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+type Props = {
+  name: string,
+  value: string
+}
+
+const props = defineProps<Props>();
+
+const { handleChange, checked } =  useField(() => props.name, undefined, {
+  type: "checkbox",
+  checkedValue: props.value
+})
+</script>
+
+<template>
+  <div class="flex items-center p-2">
+  <input
+    :checked="checked"
+    class="m-0 mr-2 h-4 w-4 border-1 border-primary rounded-100 border-solid text-primary ring-primary focus:ring-primary"
+    type="checkbox"
+    :value="value"
+    @change="handleChange"/>
+
+    <label class="pr-2 text-xs text-outline">
+    <slot />
+    </label>
+  </div>
+</template>
