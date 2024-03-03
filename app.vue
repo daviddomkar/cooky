caecb7c38ae2a4fb81bc2c954173a2fc3497a2b3
Martin Kaněra <marti.kanera@gmail.com>
2024-03-03 20:25:35 +0100

    feat: add auth layout, sign up and log in pages

--- app.vue
+++ app.vue
@@ -1,19 +1,7 @@
-<script setup lang="ts">
-const auth = useAuth();
-
-console.log(auth.session.value);
-
-const signIn = () => {
-  auth.signIn("credentials", {
-    usernameOrEmail: "keyqlin",
-    password: "davidek",
-  });
-};
-</script>
-
 <template>
   <div>
-    <h1>Hello World!</h1>
-    <button @click="signIn">Show Alert</button>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
