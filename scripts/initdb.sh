b2705cb57e9e14c3968ea9f9e46fd45620cd9025
David Domkář <david@domkar.dev>
2024-03-04 12:05:56 +0100

    fix(docker): add initdb.sh script

---
+++ scripts/initdb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER "$COOKY_USER" WITH PASSWORD '$COOKY_PASSWORD';
+    GRANT ALL PRIVILEGES ON DATABASE "$POSTGRES_DB" TO "$COOKY_USER";
+EOSQL
+
+export DATABASE_URL="postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:5432/$POSTGRES_DB?host=/var/run/postgresql/"
+
+echo "Downloading required dependencies, this may take a while..."
+cd docker-entrypoint-initdb.d && npm ci
+
+echo "Setting up database..."
+npm run prisma:migrate:reset -- --force
