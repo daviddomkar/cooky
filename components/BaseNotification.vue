5c2c94381730f34b2080d9889410cf4b0eb2b73b
Karolína Chladíková <chladikovak@gmail.com>
2024-03-04 15:41:26 +0100

    feat(components): base notification init

---
+++ components/BaseNotification.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex">
+    <div class="i-material-symbols-error"></div>
+    <div>
+      <h3></h3>
+      <div></div>
+    </div>
+    <div></div>
+  </div>
+</template>
