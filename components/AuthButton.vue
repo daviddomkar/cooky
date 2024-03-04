70e8a2c44a7fa000769ce071f2c669a051420ec7
Karolína Chladíková <chladikovak@gmail.com>
2024-03-04 12:53:54 +0100

    feat(components): init authButton
    Nefunkční interaktivní barva u zaoblení dark/light theme, nutnost vymyslet jinak

---
+++ components/AuthButton.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+type Props = {
+  active: boolean;
+};
+
+const props = withDefaults(defineProps<Props>(), {
+  active: true,
+});
+
+const activeAuthButton = computed(() => {
+  const defaultClass =
+    "authbutton relative box-border h-24 w-64 flex items-center justify-center rounded-l-full border-none bg-surface px-8 py-3 text-2xl  uppercase cursor-pointer ";
+  return (
+    defaultClass +
+    (props.active
+      ? "bg-transparent text-white"
+      : "bg-surface after:absolute before:absolute after:right-0 before:right-0 after:h-16 after:w-16 before:h-16 before:w-16 text-on-surface after:bg-surface before:bg-surface after:content-[''] before:content-[''] after:-bottom-16 before:-top-16")
+  );
+});
+</script>
+
+<template>
+  <button :class="activeAuthButton">
+    <span>
+      <slot />
+    </span>
+  </button>
+</template>
+
+<style scoped>
+.authbutton::before {
+  background: radial-gradient(64px at 0 0, transparent 99.7%, #fff);
+}
+
+.authbutton::after {
+  background: radial-gradient(64px at 0 100%, transparent 99.7%, #fff);
+}
+</style>
+```
