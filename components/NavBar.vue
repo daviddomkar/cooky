344fe68ce4b15fa2e60eb73bc177db251c886ae8
Matyanson <matyas.perem@seznam.cz>
2024-03-17 14:16:59 +0100

    chore: basic structure

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -14,10 +14,21 @@ const categoryTitles = ref(["BREAKFAST", "LUNCH", "DINNER", "SNACKS", "SOUPS", "
 
 <template>
   <nav>
+    <div class="gradient"></div>
+    <div class="strip"></div>
+    <svg id="svg">
+      <clipPath id="clip">
+        <rect id="hole" height="0" width="0"/>
+        <path id="skirt"/>
+      </clipPath>
+    </svg>
+  <div class="content">
+    <div class="title">Cooky</div>
     <ul>
       <li v-for="category in categoryTitles" :key="category">
         {{ category }}
       </li>
     </ul>
-  </nav>
+  </div>
+</nav>
 </template>
