8b728181b93ae4c3f0998776a3798c9ed0699be1
Karolína Chladíková <chladikovak@gmail.com>
2024-03-25 16:50:52 +0100

    feat(icons): add icons (admin)

--- components/CustomIcons.vue
+++ components/CustomIcons.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex gap-12">
+  <div class="grid grid-cols-4 gap-12">
     <div class="i-cooky:breakfast h-24 w-24" />
     <div class="i-cooky:dinner h-24 w-24" />
     <div class="i-cooky:lunch h-24 w-24" />
@@ -9,8 +9,16 @@
     <div class="i-cooky:search h-24 w-24" />
     <div class="i-cooky:favourites h-24 w-24" />
     <div class="i-cooky:hamburger h-24 w-24" />
-    <div class="i-cooky:recipes h-24 w-24" />
+    <div class="i-cooky:my-recipes h-24 w-24" />
     <div class="i-cooky:notification h-24 w-24" />
     <div class="i-cooky:print h-24 w-24" />
+    <div class="i-cooky:ingredients h-24 w-24" />
+    <div class="i-cooky:list h-24 w-24" />
+    <div class="i-cooky:logout h-24 w-24" />
+    <div class="i-cooky:pencil h-24 w-24" />
+    <div class="i-cooky:recipes h-24 w-24" />
+    <div class="i-cooky:reviews h-24 w-24" />
+    <div class="i-cooky:roles h-24 w-24" />
+    <div class="i-cooky:users h-24 w-24" />
   </div>
 </template>
