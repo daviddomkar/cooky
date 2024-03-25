74f6b9f6c96ec5ecf5897add022d190556ff2724
Matyanson <matyas.perem@seznam.cz>
2024-03-25 16:37:51 +0100

    feat(navbar/storybook): replace NuxtLink with anchor

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,6 +7,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-themes",
+    "@storybook/addon-actions",
   ],
   framework: {
     name: "@storybook-vue/nuxt",
