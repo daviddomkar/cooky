488caa0e57b0f2dec4bb970f6609d778e65e266c
Karolína Chladíková <chladikovak@gmail.com>
2024-03-28 19:25:40 +0100

    fix(views): fix SQL & define views prisma

--- prisma/migrations/20240313104224_add_views/migration.sql
+++ prisma/migrations/20240313104224_add_views/migration.sql
@@ -1,25 +1,25 @@
 -- Create views
 
 CREATE OR REPLACE VIEW recipes_sorted_by_ratings AS
-SELECT ratings.recipe_id AS recipe_id, avg(number_of_stars) AS average_stars
-FROM ratings
-    NATURAL JOIN recipes
-GROUP BY ratings.recipe_id
-ORDER BY avg(number_of_stars)
+SELECT recipes.*, coalesce(avg(number_of_stars), 0) as average_stars
+FROM recipes
+    LEFT JOIN ratings ON recipes.id = ratings.recipe_id
+GROUP BY recipes.id
+ORDER BY average_stars
 DESC;
 
 CREATE OR REPLACE VIEW recipes_sorted_by_comments AS
-SELECT recipes, COUNT(comments.id) as comment_count
+SELECT recipes.*, count(comments.id) as comment_count
 FROM comments
 JOIN recipes ON comments.recipe_id = recipes.id
 GROUP BY recipes.id
 ORDER BY comment_count DESC;
 
 CREATE OR REPLACE VIEW recipes_sorted_by_saves AS
-SELECT r.id, COUNT(rl.recipe_id) AS save_count
-FROM recipes r
-LEFT JOIN recipe_lists rl ON r.id = rl.recipe_id
-GROUP BY r.id
+SELECT recipes.*, coalesce(count(recipes.id), 0) as save_count
+FROM recipes
+LEFT JOIN recipe_lists ON recipes.id = recipe_lists.recipe_id
+GROUP BY recipes.id
 ORDER BY save_count DESC;
 
 CREATE OR REPLACE VIEW recipes_sorted_by_creation_date AS
@@ -29,8 +29,8 @@ ORDER BY created_at DESC;
 
 -- Random recipe
 CREATE OR REPLACE VIEW random_recipe AS
-SELECT r.id, r.title
-FROM recipes r
+SELECT *
+FROM recipes
 ORDER BY RANDOM()
 LIMIT 1;
 
@@ -38,11 +38,11 @@ LIMIT 1;
 CREATE OR REPLACE FUNCTION get_random_recipe_from_category(p_category_id categories.id%TYPE)
 RETURNS SETOF recipes AS $$
 BEGIN
-  RETURN QUERY SELECT r.*
-  FROM recipes r
-  JOIN recipe_categories rc ON r.id = rc.recipe_id
-  JOIN categories c ON rc.category_id = c.id
-  WHERE c.id = p_category_id
+  RETURN QUERY SELECT recipes.*
+  FROM recipes
+  JOIN recipe_categories ON recipes.id = recipe_categories.recipe_id
+  JOIN categories ON recipe_categories.category_id = categories.id
+  WHERE categories.id = p_category_id
   ORDER BY RANDOM()
   LIMIT 1;
 END;
