94bf8eb782b4d73d0edc86cf8ccc5df4a60c9a62
Matyanson <matyas.perem@seznam.cz>
2024-04-04 17:30:07 +0200

    feat(recipe-page): init

--- pages/[username]/[slug].vue
+++ pages/[username]/[slug].vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
-const route = useRoute();
+  const route = useRoute();
+  const { username, slug } = route.params;
+  const { data } = await useFetch(`/api/profile/${username}`);
+  const recipeData = data.value?.recipes.find(x => x.slug === slug);
+
 </script>
 
 <template>
-  <div>{{ route.params }}</div>
+  <div>
+    <div class="header flex">
+      <img :src="`/api/files/${recipeData?.imageId}`"/>
+    </div>
+    <div class="body">
+      {{ route.params }}
+      {{ slug }}
+      {{ data }}
+    </div>
+  </div>
 </template>
