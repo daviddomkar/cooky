0e19b23bbca5badd84ed14d1af77af95179c3cd0
Matyanson <matyas.perem@seznam.cz>
2024-04-07 15:52:41 +0200

    feat(recipe-page): header

--- pages/[username]/[slug].vue
+++ pages/[username]/[slug].vue
@@ -4,6 +4,8 @@
   const { data: userData } = await useFetch(`/api/profile/${username}`);
   const { data: recipeData } = await useFetch(`/api/recipes/${username}/${slug}`);
 
+  console.log(recipeData.value);
+
 </script>
 
 <template>
@@ -13,24 +15,34 @@
   >
     <div class="header flex flex-row p-5">
       <RecipeImage class="flex-shrink-0" :image-id="recipeData.imageId" />
-      <div class="info flex flex-col p-5">
+      <div class="info flex flex-col justify-start p-5">
         <div class="info-header flex flex-col">
-          <div class="flex flex-row items-center"> <PictureFrame
-            borderless
-            class="w-8 shrink-0 cursor-pointer sm:w-12"
-            :src="getProfileImageUrl(userData.username, userData.profileImageId)"
-          /> {{ userData?.username }}
+          <div class="flex flex-row items-center">
+            <PictureFrame
+              borderless
+              class="mr-3 w-5 shrink-0 cursor-pointer sm:w-8"
+              :src="getProfileImageUrl(userData.username, userData.profileImageId)"
+            /> {{ userData?.username }}
           </div>
-          <div class="flex flex-row items-center font-size-7 font-display">
+          <div class="flex flex-row items-center text-3xl font-display">
+            <!-- GET RATING -->
             <h2>{{ recipeData.title }}</h2><div>4,5/5</div>
           </div>
         </div>
-        <div class="description">
-            <p>
-              {{ recipeData.description }}
-            </p>
-            {{ recipeData }}
-          </div>
+        <p class="description">
+          {{ recipeData.description }}
+        </p>
+        <ul class="p-0">
+          <li class="flex flex-row items-center">
+            <span class="mr-3 text-xl font-bold font-display">PREP TIME</span>
+            <!-- GET PREP DURATION -->
+            40 minutes
+          </li>
+          <li class="flex flex-row items-center">
+            <span class="mr-3 text-xl font-bold font-display">NUTRITION / SERV</span>
+            {{ recipeData.nutritionPerServing }} kcal
+          </li>
+        </ul>
       </div>
     </div>
     <div class="body">
