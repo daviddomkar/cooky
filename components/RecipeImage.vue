5a1241414b844b1e95a4bffec2a061f2179a9695
Matyanson <matyas.perem@seznam.cz>
2024-04-07 15:09:10 +0200

    feat(recipe-page): recipe-image component

---
+++ components/RecipeImage.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+type Props = {
+  imageId: string;
+};
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="relative h-100 overflow-hidden rounded-3xl sm:h-90 sm:w-60">
+    <img
+      class="block h-full w-full object-cover object-center"
+      :src="`/api/files/${imageId}`"
+    />
+  </div>
+</template>
