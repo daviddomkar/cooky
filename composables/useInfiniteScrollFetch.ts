c24d8b4e40d832f18e6fe570cc92574a37378b29
David Domkář <david@domkar.dev>
2024-04-11 03:37:42 +0200

    feat(infinite-scroll): improve composable

--- composables/useInfiniteScrollFetch.ts
+++ composables/useInfiniteScrollFetch.ts
@@ -1,5 +1,9 @@
 import type { UseInfiniteScrollOptions } from "@vueuse/core";
-import type { UseFetchOptions, FetchResult } from "nuxt/app";
+import type {
+  UseFetchOptions,
+  FetchResult,
+  AsyncDataRequestStatus,
+} from "nuxt/app";
 import type { FetchError } from "ofetch";
 import type { NitroFetchRequest, AvailableRouterMethod } from "nitropack";
 
@@ -66,11 +70,17 @@ export async function useInfiniteScrollFetch<
   opts: UseInfiniteScrollFetchOptions<ReqT, DataT, PickKeys, E> = {},
 ): Promise<{
   data: Ref<DataT["results"]>;
+  status: Ref<AsyncDataRequestStatus>;
+  error: Ref<ErrorT | null>;
 }> {
   const after = ref<string | undefined | null>(null);
-  const results = ref([]) as Ref<DataT["results"]>;
+  const data = ref([]) as Ref<DataT["results"]>;
 
-  const { data: currentData, status } = await useFetch<
+  const {
+    data: currentData,
+    status,
+    error,
+  } = await useFetch<
     DataT,
     ErrorT,
     ReqT,
@@ -90,11 +100,10 @@ export async function useInfiniteScrollFetch<
 
   const currentAfter = computed(() => (currentData.value as DataT).after);
 
-  results.value = (currentData.value as DataT).results;
+  data.value = (currentData.value as DataT).results;
 
   watch(currentData, (newData) => {
-    const data = newData as DataT;
-    results.value = [...results.value, ...data.results];
+    data.value = [...data.value, ...(newData as DataT).results];
   });
 
   useInfiniteScroll(
@@ -109,61 +118,8 @@ export async function useInfiniteScrollFetch<
   );
 
   return {
-    data: results,
-  };
-}
-
-export const useInfiniteScrollFetchOld = <T>(
-  url: string,
-  el: Ref<HTMLElement | null>,
-  providedQuery: MaybeRefOrGetter<PaginationQuery> = {},
-  infiniteScrollOptions: UseInfiniteScrollOptions = {},
-) => {
-  const nextCursor = ref<string | undefined>();
-  const data = ref<T[]>([]) as Ref<T[]>;
-  const error = ref<any>(undefined);
-  const hasMore = ref(true);
-  const isFetching = ref(false);
-  const query = ref(providedQuery);
-
-  watch(
-    () => toValue(query),
-    async (changedQuery) => {
-      query.value = changedQuery;
-      data.value = [];
-      nextCursor.value = undefined;
-      hasMore.value = true;
-      isFetching.value = true;
-      await infiniteScrollCallback();
-      isFetching.value = false;
-    },
-  );
-
-  const infiniteScrollCallback = async () => {
-    try {
-      const result = await $fetch<PaginationResult<T>>(url, {
-        query: {
-          ...query.value,
-          nextCursor: nextCursor.value,
-        },
-      });
-
-      data.value?.push(...result.data);
-      nextCursor.value = result.pagination.nextCursor;
-      hasMore.value = !!result.pagination.nextCursor;
-    } catch (e) {
-      error.value = e;
-    }
-  };
-
-  const { isLoading } = useInfiniteScroll(el, infiniteScrollCallback, {
-    ...infiniteScrollOptions,
-    canLoadMore: () => hasMore.value && !isFetching.value,
-  });
-
-  return {
     data,
-    pending: isLoading,
+    status,
     error,
   };
-};
+}
