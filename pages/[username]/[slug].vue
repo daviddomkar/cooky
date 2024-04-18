0eda6c7500617aaf01cf17b130fb3aaaff5d66f8
Matyanson <matyas.perem@seznam.cz>
2024-04-18 18:04:19 +0200

    chore(recipe-page): formating

--- pages/[username]/[slug].vue
+++ pages/[username]/[slug].vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
-  const route = useRoute();
-  const { username, slug } = route.params;
-  const { data: userData } = await useFetch(`/api/profile/${username}`);
-  const { data: recipeData } = await useFetch(`/api/recipes/${username}/${slug}`);
+const route = useRoute();
+const { username, slug } = route.params;
+const { data: userData } = await useFetch(`/api/profile/${username}`);
+const { data: recipeData } = await useFetch(`/api/recipes/${username}/${slug}`);
 
-  const processInterval = (x: string) => x
-          .split(":")
-          .map((x) => Number(x))
-          .map((x, i) => (x === 0 ? "" : `${x}${["h", "m", "s"][i]}`))
-          .join("");
-
+const processInterval = (x: string) =>
+  x
+    .split(":")
+    .map((x) => Number(x))
+    .map((x, i) => (x === 0 ? "" : `${x}${["h", "m", "s"][i]}`))
+    .join("");
 </script>
 
 <template>
-   <main
+  <main
     v-if="recipeData"
     class="m-auto min-h-[calc(100vh-96px)] w-full flex flex-col justify-center gap-8 py-24"
   >
@@ -25,13 +25,21 @@
             <PictureFrame
               borderless
               class="w-5 shrink-0 cursor-pointer sm:w-8"
-              :src="getProfileImageUrl(userData.username, userData.profileImageId)"
-            /> {{ userData?.username }}
+              :src="
+                getProfileImageUrl(userData.username, userData.profileImageId)
+              "
+            />
+            {{ userData?.username }}
           </div>
-          <div class="flex flex-row flex-wrap items-center gap-3 text-3xl font-display">
+          <div
+            class="flex flex-row flex-wrap items-center gap-3 text-3xl font-display"
+          >
             <h2>{{ recipeData.title }}</h2>
             <div>
-              <RecipeStars :model-value="recipeData.rating ?? undefined" variant="display" />
+              <RecipeStars
+                :model-value="recipeData.rating ?? undefined"
+                variant="display"
+              />
             </div>
           </div>
         </div>
@@ -48,14 +56,20 @@
             {{ recipeData.nutritionPerServing }} kcal
           </li>
         </ul>
-        <div class="options mt-auto flex flex-row items-center justify-end gap-3">
+        <div
+          class="options mt-auto flex flex-row items-center justify-end gap-3"
+        >
           <BaseButton spread="compact" variant="secondary">
-            <div class="i-cooky:print" />
+            <template #icon>
+              <div class="i-cooky:print" />
+            </template>
           </BaseButton>
-          <BaseButton expanded spread="compact">
-            <div class="i-cooky:favourites" />
-          SAVE
-        </BaseButton>
+          <BaseButton spread="compact">
+            <template #icon>
+              <div class="i-cooky:favourites" />
+            </template>
+            SAVE
+          </BaseButton>
         </div>
       </div>
     </div>
@@ -63,14 +77,19 @@
       <div class="ingredients flex flex-col sm:flex-shrink-0">
         <div class="title flex flex-row items-center gap-3">
           <h2>INGREDIENTS</h2>
-          <h3 >SERV</h3>
+          <h3>SERV</h3>
           {{ recipeData.numberOfServings }}
         </div>
         <ul class="list-none p-0">
-        <li v-for="(ingredient, i) in recipeData.ingredients" :key="i" class="mb-3">
-          {{ ingredient.amount }} {{ ingredient.unit.abbreviation }} {{ ingredient.ingredient.title }}
-        </li>
-      </ul>
+          <li
+            v-for="(ingredient, i) in recipeData.ingredients"
+            :key="i"
+            class="mb-3"
+          >
+            {{ ingredient.amount }} {{ ingredient.unit.abbreviation }}
+            {{ ingredient.ingredient.title }}
+          </li>
+        </ul>
       </div>
       <div class="steps flex flex-col">
         <h2>STEPS</h2>
@@ -87,14 +106,16 @@
           <PictureFrame
             borderless
             class="w-5 shrink-0 cursor-pointer sm:w-12"
-            :src="getProfileImageUrl(userData.username, userData.profileImageId)"
+            :src="
+              getProfileImageUrl(userData.username, userData.profileImageId)
+            "
           />
           <TextField label="Add a comment..." name="comment" type="text" />
         </div>
       </div>
       <ul>
         <li v-for="comment in recipeData.comments" :key="comment.id">
-          <RecipeComment 
+          <RecipeComment
             :author-image-id="comment.author.profileImageId"
             :author-name="comment.author.username"
             class="mb-3"
@@ -103,7 +124,7 @@
           />
           <ul>
             <li v-for="reply in comment.replies" :key="reply.id">
-              <RecipeComment 
+              <RecipeComment
                 :author-image-id="reply.author.profileImageId"
                 :author-name="reply.author.username"
                 class="mb-3"
