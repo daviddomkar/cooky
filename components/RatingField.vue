f7914021c379414a0ccf60a04500bf1108a1ce0d
David Domkář <david@domkar.dev>
2024-04-21 15:10:54 +0200

    feat(recipe): add rating

--- components/RatingField.vue
+++ components/RatingField.vue
@@ -40,56 +40,60 @@ const setValue = (value: number) => {
 </script>
 
 <template>
-  <div class="max-w-60 w-60 flex flex-row items-end justify-between gap-4">
-    <div
-      class="group flex flex-row flex-nowrap"
-      :class="{
-        'cursor-pointer': editable,
-      }"
-      @mouseleave="hoverValue = null"
-    >
+  <div class="max-w-60 w-60 flex flex-row items-center justify-between">
+    <div class="grow">
       <div
-        v-for="i in 5"
-        :key="i"
-        class="box-border px-1.5 text-xl"
+        class="group flex flex-row flex-nowrap"
         :class="{
-          'pl-0': i === 1,
-          'pr-0': i === 5,
+          'cursor-pointer': editable,
         }"
-        @click="setValue(i)"
-        @mouseover="setHoverValue(i)"
+        @mouseleave="hoverValue = null"
       >
         <div
-          class="i-cooky:star relative"
+          v-for="i in 5"
+          :key="i"
+          class="box-border px-1.5 text-xl"
           :class="{
-            '!text-primary': roundedValue >= i || hoverValue,
-            'group-hover:text-primary': editable && roundedValue >= i,
-            'text-on-surface-variant dark:text-outline': roundedValue < i,
-            'group-hover:!text-on-surface-variant dark:group-hover:!text-outline':
-              hoverValue ? hoverValue < i : false,
+            'pl-0': i === 1,
+            'pr-0': i === 5,
           }"
+          @click="setValue(i)"
+          @mouseover="setHoverValue(i)"
         >
           <div
-            v-if="!hoverValue && editable"
-            class="absolute inset-0 box-border w-50% overflow-hidden"
+            class="i-cooky:star relative"
+            :class="{
+              '!text-primary': roundedValue >= i || hoverValue,
+              'group-hover:text-primary': editable && roundedValue >= i,
+              'text-on-surface-variant dark:text-outline': roundedValue < i,
+              'group-hover:!text-on-surface-variant dark:group-hover:!text-outline':
+                hoverValue ? hoverValue < i : false,
+            }"
           >
             <div
-              class="i-cooky:starhalf"
-              :class="{
-                'text-primary': roundedValue >= i - 0.5,
-                'text-on-surface-variant dark:text-outline':
-                  roundedValue < i - 0.5,
-              }"
-            />
+              v-if="!hoverValue && editable"
+              class="absolute inset-0 box-border w-50% overflow-hidden"
+            >
+              <div
+                class="i-cooky:starhalf"
+                :class="{
+                  'text-primary': roundedValue >= i - 0.5,
+                  'text-on-surface-variant dark:text-outline':
+                    roundedValue < i - 0.5,
+                }"
+              />
+            </div>
           </div>
         </div>
       </div>
     </div>
+
     <p
       v-show="hoverValue ?? roundedValue"
       class="my-0 text-nowrap leading-none"
     >
       {{ hoverValue ? hoverValue : roundedValue }} / 5
     </p>
+    <slot name="trailing" />
   </div>
 </template>
--- pages/[author]/[slug].vue
+++ pages/[author]/[slug].vue
@@ -9,9 +9,11 @@ const { notify } = useNotification();
 
 const { session } = useAuth();
 
-const { data: recipe, error } = await useFetch(
-  `/api/recipes/${route.params.author}/${route.params.slug}`,
-);
+const {
+  data: recipe,
+  refresh: refreshRecipe,
+  error,
+} = await useFetch(`/api/recipes/${route.params.author}/${route.params.slug}`);
 
 if (error.value) {
   throw createError({
@@ -19,6 +21,10 @@ if (error.value) {
     statusMessage: error.value?.statusMessage,
   });
 }
+
+const { data: rating, refresh: refreshRating } = await useFetch(
+  `/api/recipes/${route.params.author}/${route.params.slug}/ratings`,
+);
 
 const { data: lists, refresh: refreshLists } = await useAsyncData(
   async () => {
@@ -50,8 +56,75 @@ const print = () => {
   window.print();
 };
 
-const rate = (value: number) => {
-  console.log("rate", value);
+const addRating = async (value: number) => {
+  try {
+    await $fetch(
+      `/api/recipes/${recipe.value?.author?.username}/${recipe.value?.slug}/ratings`,
+      {
+        method: "POST",
+        body: {
+          numberOfStars: value,
+        },
+      },
+    );
+
+    await Promise.all([refreshRating(), refreshRecipe()]);
+
+    notify({
+      type: "success",
+      title: `Recipe ${recipe.value?.title} rated.`,
+      text: `You rated the recipe with ${value} stars.`,
+    });
+  } catch (e) {
+    if (e instanceof FetchError) {
+      notify({
+        type: "error",
+        title: "Failed to rate recipe.",
+        text: e.message,
+      });
+      return;
+    }
+
+    notify({
+      type: "error",
+      title: "Failed to rate recipe.",
+      text: "An unknown error occurred.",
+    });
+  }
+};
+
+const removeRating = async () => {
+  try {
+    await $fetch(
+      `/api/recipes/${recipe.value?.author?.username}/${recipe.value?.slug}/ratings`,
+      {
+        method: "DELETE",
+      },
+    );
+
+    await Promise.all([refreshRating(), refreshRecipe()]);
+
+    notify({
+      type: "success",
+      title: `Recipe ${recipe.value?.title} unrated.`,
+      text: `You deleted your rating from this recipe.`,
+    });
+  } catch (e) {
+    if (e instanceof FetchError) {
+      notify({
+        type: "error",
+        title: "Failed to unrate recipe.",
+        text: e.message,
+      });
+      return;
+    }
+
+    notify({
+      type: "error",
+      title: "Failed to unrate recipe.",
+      text: "An unknown error occurred.",
+    });
+  }
 };
 
 const deleteRecipe = async () => {
@@ -99,7 +172,9 @@ const deleteRecipe = async () => {
         class="block w-full shrink-0 rounded-3xl print:hidden lg:min-w-80 lg:w-80"
         :src="`/api/files/${recipe.imageId}`"
       />
-      <div class="mx-auto flex flex-col self-stretch gap-2 lg:mx-0 lg:grow">
+      <div
+        class="mx-auto flex flex-col items-center self-stretch gap-2 lg:mx-0 lg:grow lg:items-start"
+      >
         <div
           class="flex flex-col items-center gap-4 lg:flex-row lg:items-start"
         >
@@ -128,15 +203,38 @@ const deleteRecipe = async () => {
             </ConfirmationDialog>
           </div>
         </div>
-        <RatingField
-          v-if="recipe.state !== RecipeState.DRAFT"
-          class="mx-auto mt-4 lg:mx-0 print:hidden"
-          :controlled="false"
-          :editable="!!session && !isOwnRecipe"
-          :model-value="recipe.rating ?? undefined"
-          name="rating"
-          @update:model-value="rate"
-        />
+        <div class="flex flex-col gap-2">
+          <RatingField
+            v-if="recipe.state !== RecipeState.DRAFT"
+            class="mx-auto mt-4 lg:mx-0 print:hidden"
+            :class="{
+              '[&>p]:text-primary': rating?.numberOfStars,
+            }"
+            :controlled="false"
+            :editable="!!session && !isOwnRecipe"
+            :model-value="rating?.numberOfStars ?? recipe.rating ?? undefined"
+            name="rating"
+            @update:model-value="addRating"
+          >
+            <template v-if="rating && recipe.rating" #trailing>
+              <BaseButton
+                v-if="rating && rating.numberOfStars"
+                class="ml-2 inline-flex vertical-top !h-4 !min-w-4"
+                density="compact"
+                spread="none"
+                variant="transparent"
+                @click="removeRating"
+              >
+                <div
+                  class="i-material-symbols:close-rounded scale-[1.25] text-error"
+                />
+              </BaseButton>
+            </template>
+          </RatingField>
+          <span v-if="rating && recipe.rating" class="self-end">
+            ({{ Math.round(recipe.rating * 2) / 2 }} global rating)
+          </span>
+        </div>
         <div class="my-4 flex justify-center lg:justify-left">
           <ul class="my-0 max-w-60 w-60 list-none pl-0">
             <li class="flex items-center">
